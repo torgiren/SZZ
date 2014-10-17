bc764a3d44eed8bf9a91778ccc4710426264b6d1
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2014-10-17 18:39:58 +0200

    kosmetyczne zmiany

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -6,20 +6,22 @@ import os
 import matplotlib.pyplot as plt
 import urllib
 quit = False
+
+
 class Requester(threading.Thread):
     def run(self):
         self.times = []
         while not quit:
             self.times.append(time.time())
 #            time.sleep(1.0 + random.random() )
-            urllib.urlopen('http://citybox.cl')
-            #urllib.request.urlopen('http://paczkomaty.pl')
-horyzont = 30
+            urllib.urlopen('http://test.pl/?fib=25')
+#            urllib.request.urlopen('http://paczkomaty.pl')
+horyzont = 60
 watki = [Requester() for i in range(50)]
 for i in watki:
     i.start()
 plt.ion()
-plt.axis([0,horyzont,0,3000])
+plt.axis([0, horyzont, 0, 3000])
 plt.show(block=False)
 start = time.time()
 try:
@@ -27,15 +29,16 @@ try:
         t = time.time()
         for i in watki:
             inter = [j for j in i.times if j > (time.time() - horyzont)]
-            inter,probki = list(map(lambda x,y: int(1000*(y-x)), inter[:-1], inter[1:])), list(map(lambda x: x - inter[0], inter[1:]))
+            inter, probki = (list(map(lambda x, y: int(1000*(y-x)), inter[:-1], inter[1:])),
+                             list(map(lambda x: x - inter[0], inter[1:])))
             inter.append(int(1000*(time.time() - i.times[-1])))
             extra = probki[-1] if len(probki) else 0
             probki.append(time.time() - i.times[-1] + extra)
-            
+
 #            inter = map(lambda x: int(1000*x), inter)
 #            print(list(inter))
 #            print(inter[:])
-#            print(probki) 
+#            print(probki)
             plt.plot(probki, inter)
 #            print(i.times)
 #            plt.scatter(time.time() - start, inter[-1],marker='.')
