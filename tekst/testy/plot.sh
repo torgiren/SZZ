125c3c35de6c932592ea81946272c6a9a5213c79
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2015-03-30 19:56:16 +0200

    closes #5 - testy lvs z wieloma real serverami

--- tekst/testy/plot.sh
+++ tekst/testy/plot.sh
@@ -14,6 +14,6 @@ do
 	set style data lines
 	set xlabel "Procent zapytan"
 	set datafile separator ","
-	plot '$1/test_10.13.0.22_${c}.csv' u 1:2 every ::51 title 'Nginx', '$1/test_10.13.0.21_${c}.csv' u 1:2 every ::51 title 'Apache2'
+	plot '$1/test_10.13.0.11_${c}.csv' u 1:2 every ::51::100 title 'Nginx', '$1/test_10.13.0.101_${c}.csv' u 1:2 every ::51::100 title 'LVS 2xRS', '$1/test_10.13.0.102_${c}.csv' u 1:2 every ::51::100 title 'LVS 3xRS', '$1/test_10.13.0.103_${c}.csv' u 1:2 every ::51::100 title 'LVS 4xRS'
 EOF
 done
