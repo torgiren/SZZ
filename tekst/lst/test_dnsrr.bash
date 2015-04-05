e1514d471f64525afabafb5420d97b122fdf97bd
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2015-04-06 00:01:10 +0200

    jakies zapomniane pliki

---
+++ tekst/lst/test_dnsrr.bash
@@ -0,0 +1,8 @@
+(default3.2)[nie 15/03/01 13:40 CET][pts/32][x86_64/linux-gnu/3.18.1-gentoo][5.0.7]
+<torgiren@redraptor:~/SZZ/tekst/lst>
+zsh/6 13647  (git)-[tekst]-% dig rr.mgr.fabrykowski.pl +short
+10.13.0.14
+10.13.0.11
+10.13.0.12
+10.13.0.13
+
