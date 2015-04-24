216a772ee9eb82b4a78ce0ac80a9b85069398511
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2015-04-24 20:06:40 +0200

    skończenie LVS

--- tekst/testy/plot.sh
+++ tekst/testy/plot.sh
@@ -14,6 +14,6 @@ do
 	set style data lines
 	set xlabel "Procent zapytan"
 	set datafile separator ","
-	plot '$1/test_10.13.0.11_${c}.csv' u 1:2 every ::51::100 title 'Nginx', '$1/test_10.13.0.101_${c}.csv' u 1:2 every ::51::100 title 'LVS 2xRS', '$1/test_10.13.0.102_${c}.csv' u 1:2 every ::51::100 title 'LVS 3xRS', '$1/test_10.13.0.103_${c}.csv' u 1:2 every ::51::100 title 'LVS 4xRS'
+	plot '$1/test_10.13.0.11_${c}.csv' u 1:2 every ::51::100 title 'Nginx', '$1/test_10.13.0.110_${c}.csv' u 1:2 every ::51::100 title 'Haproxy 1xRS', '$1/test_10.13.0.111_${c}.csv' u 1:2 every ::51::100 title 'Haproxy 2xRS', '$1/test_10.13.0.112_${c}.csv' u 1:2 every ::51::100 title 'Haproxy 3xRS', '$1/test_10.13.0.113_${c}.csv' u 1:2 every ::51::100 title 'Haproxy 4xRS'
 EOF
 done
