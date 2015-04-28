8771ae237159b6c8ca3ba9bb90474a60f268420a
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2015-04-28 04:44:06 +0200

    testy. chyba wszystkie

--- tekst/testy/test.sh
+++ tekst/testy/test.sh
@@ -5,7 +5,7 @@ then
     exit 1
 fi
 mkdir $1
-for s in {10.13.0.11,10.13.0.110,10.13.0.111,10.13.0.112,10.13.0.113}
+for s in {10.13.0.110,10.13.0.111,10.13.0.112,10.13.0.113}
 do
     for i in {0..8}
     do
@@ -15,4 +15,6 @@ do
 		#/tmp/httpd-2.4.12/support/ab -k -r -l -n10000 -c $conc -e ./$1/test_${s}_${conc}.csv http://$s/small.html > ./$1/test_${s}_${conc}.txt
 	#	ab  -n10000 -c $conc -e ./$1/test_${s}_${conc}.csv http://$s/index.html > ./$1/test_${s}_${conc}.txt
     done
+	echo "zrobilem $s"
+	read
 done
